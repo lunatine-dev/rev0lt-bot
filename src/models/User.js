e224c6a555df8337d745d4ec1447680eb070cb02
Morgan Evans <morgan@morganevans.dev>
2023-12-23 06:07:18 +0000

    User schema

--- src/models/User.js
+++ src/models/User.js
@@ -1,1 +1,23 @@
 const mongoose = require("mongoose");
+
+const schema = new mongoose.Schema({
+    identifier: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    avatar: {
+        type: String,
+    },
+    points: {
+        type: Number,
+        default: 0,
+        min: 0,
+    },
+});
+
+module.exports = mongoose.model("User", schema);
